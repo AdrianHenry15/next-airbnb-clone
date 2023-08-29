619bc9f1647ac9669512b62e12c0e7698604112a
Adrian <adrianhenry2115@gmail.com>
2023-08-29 09:58:00 -0400

    next auth added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,29 @@
-import "./globals.css"
-import type { Metadata } from "next"
-import { Nunito } from "next/font/google"
-import Navbar from "./components/navbar/Navbar"
+import './globals.css'
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import Navbar from './components/navbar/Navbar'
+import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
 
 export const metadata: Metadata = {
-  title: "Airbnb",
-  description: "Airbnb Clone",
+    title: 'Airbnb',
+    description: 'Airbnb Clone',
 }
 
-const font = Nunito({ subsets: ["latin"] })
+const font = Nunito({ subsets: ['latin'] })
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={font.className}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={font.className}>
+                <RegisterModal />
+                <Navbar />
+                {children}
+            </body>
+        </html>
+    )
 }
